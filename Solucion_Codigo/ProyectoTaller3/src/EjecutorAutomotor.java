6eacd770433622c0b0373e778d926af14863de8f
Paulo Bustamante <pbustamante@utpl.edu.ec>
2025-06-01 17:33:03 -0500

    correccion

--- Solucion_Codigo/ProyectoTaller3/src/EjecutorAutomotor.java
+++ Solucion_Codigo/ProyectoTaller3/src/EjecutorAutomotor.java
@@ -10,81 +10,71 @@ import java.util.Scanner;
  * @author Diego
  */
 public class EjecutorAutomotor {
-    
-        
-        public static Auto auto;
 
-        public static void main(String[] args) {
-            Scanner entrada = new Scanner(System.in);
+    public static Auto auto;
 
-            System.out.print("Ingrese la cedula del dueño: ");
-            int cedula = entrada.nextInt();
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
 
-            entrada.nextLine(); 
+        System.out.print("Ingrese la cedula del dueño: ");
+        int cedula = entrada.nextInt();
 
-            System.out.print("Ingrese la marca del vehiculo: ");
-            String marca = entrada.nextLine();
+        entrada.nextLine();
 
-            System.out.print("Ingrese el año de fabricacion: ");
-            int anoFabricacion = entrada.nextInt();
+        System.out.print("Ingrese la marca del vehiculo: ");
+        String marca = entrada.nextLine();
 
-            System.out.print("Ingrese el valor del vehiculo: ");
-            double valorVehiculo = entrada.nextDouble();
+        System.out.print("Ingrese el año de fabricacion: ");
+        int anoFabricacion = entrada.nextInt();
 
-            
-            auto = new Auto(marca, valorVehiculo, anoFabricacion, cedula);
+        System.out.print("Ingrese el valor del vehiculo: ");
+        double valorVehiculo = entrada.nextDouble();
 
-        
-            auto.calcularAnoDeAntiguedad();
-            auto.calcularValorMatricula();
+        auto = new Auto(marca, valorVehiculo, anoFabricacion, cedula);
 
-            System.out.println("\nInformacion del Vehiculo: ");
-            System.out.println(auto);
+        auto.calcularAnoDeAntiguedad();
+        auto.calcularValorMatricula();
 
-           
-        }
+        System.out.println("\nInformacion del Vehiculo: ");
+        System.out.println(auto);
+
+    }
+}
+
+class Auto {
+
+    public int cedula;
+    public int anoFabricacion;
+    public double valorVehiculo;
+    public String marca;
+    public double valorMatricula;
+    public int anoDeAntiguedad;
+
+    public Auto(String marca, double valorVehiculo,
+            int anoFabricacion, int cedula) {
+        this.marca = marca;
+        this.valorVehiculo = valorVehiculo;
+        this.anoFabricacion = anoFabricacion;
+        this.cedula = cedula;
     }
 
-
-    class Auto {
-
-        public int cedula;
-        public int anoFabricacion;
-        public double valorVehiculo;
-        public String marca;
-        public double valorMatricula;
-        public int anoDeAntiguedad;
-
-     
-        public Auto(String marca, double valorVehiculo,
-                int anoFabricacion, int cedula) {
-            this.marca = marca;
-            this.valorVehiculo = valorVehiculo;
-            this.anoFabricacion = anoFabricacion;
-            this.cedula = cedula;
-        }
-
-      
-        public void calcularAnoDeAntiguedad() {
-            this.anoDeAntiguedad = 2025 - this.anoFabricacion;
-        }
-
-    
-        public void calcularValorMatricula() {
-            this.valorMatricula = 0.002 * this.valorVehiculo * this.anoDeAntiguedad;
-        }
-
-        
-        @Override
-        public String toString() {
-            return String.format("Marca del vehiculo: %s\n"
-                    + "Cedula del propietario: %d\n"
-                    + "Año de fabricacion: %d\n"
-                    + "Años de antigüedad: %d\n"
-                    + "Valor del vehiculo: %.2f\n"
-                    + "Valor de la matricula: %.2f",
-                    marca, cedula, anoFabricacion,
-                    anoDeAntiguedad, valorVehiculo, valorMatricula);
-        }
+    public void calcularAnoDeAntiguedad() {
+        this.anoDeAntiguedad = 2025 - this.anoFabricacion;
     }
 
+    public void calcularValorMatricula() {
+        this.valorMatricula = 0.002 * this.valorVehiculo * this.anoDeAntiguedad;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Marca del vehiculo: %s\n"
+                + "Cedula del propietario: %d\n"
+                + "Año de fabricacion: %d\n"
+                + "Años de antigüedad: %d\n"
+                + "Valor del vehiculo: %.2f\n"
+                + "Valor de la matricula: %.2f",
+                marca, cedula, anoFabricacion,
+                anoDeAntiguedad, valorVehiculo, valorMatricula);
+    }
+}
--- Solucion_Codigo/ProyectoTaller3/src/EjecutorDispElectr.java
+++ Solucion_Codigo/ProyectoTaller3/src/EjecutorDispElectr.java
@@ -27,8 +27,8 @@ public class EjecutorDispElectr {
 
         System.out.print("Ingrese el IVA en porcentaje: ");
         double ivaPorcentaje = entrada.nextDouble();
-        
-        entrada.nextLine(); 
+
+        entrada.nextLine();
 
         System.out.print("Ingrese la dirección MAC de su dispositivo: ");
         String dirMac = entrada.nextLine();
@@ -36,21 +36,17 @@ public class EjecutorDispElectr {
         System.out.print("Ingrese el IMEI de su dispositivo móvil: ");
         String imeI = entrada.nextLine();
 
-        
-        celulares = new EquipoCelular(sistemaOp, tamano, costoI, 
+        celulares = new EquipoCelular(sistemaOp, tamano, costoI,
                 ivaPorcentaje, dirMac, imeI);
 
-        
         celulares.calcularIvaCostoInicial();
         celulares.calcularCostoFinal();
 
         System.out.println("\n=== Información del Equipo Celular ===");
         System.out.println(celulares);
 
-      
     }
 }
-
 
 class EquipoCelular {
 
@@ -63,11 +59,9 @@ class EquipoCelular {
     public String dirMac;
     public String imeI;
 
-  
     public EquipoCelular() {
     }
 
-    
     public EquipoCelular(String sistemaOp, String tamano, double costoI, double ivaPorcentaje, String dirMac, String imeI) {
         this.sistemaOp = sistemaOp;
         this.tamano = tamano;
@@ -77,27 +71,24 @@ class EquipoCelular {
         this.imeI = imeI;
     }
 
-  
     public void calcularIvaCostoInicial() {
         this.ivaCostoInicial = (this.costoI * this.ivaPorcentaje) / 100;
     }
 
-    
     public void calcularCostoFinal() {
         this.costoFinal = this.costoI + this.ivaCostoInicial;
     }
 
-  
     @Override
     public String toString() {
         return String.format("Sistema Operativo: %s\n"
-                            + "Tamaño de Pantalla: %s\n"
-                            + "Costo Inicial: %.2f\n"
-                            + "IVA Porcentaje: %.2f%%\n"
-                            + "IVA sobre Costo Inicial: %.2f\n"
-                            + "Costo Final: %.2f\n"
-                            + "Dirección MAC: %s\n"
-                            + "IMEI: %s",
+                + "Tamaño de Pantalla: %s\n"
+                + "Costo Inicial: %.2f\n"
+                + "IVA Porcentaje: %.2f%%\n"
+                + "IVA sobre Costo Inicial: %.2f\n"
+                + "Costo Final: %.2f\n"
+                + "Dirección MAC: %s\n"
+                + "IMEI: %s",
                 sistemaOp,
                 tamano,
                 costoI,
